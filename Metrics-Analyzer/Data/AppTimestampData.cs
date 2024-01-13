db199aee50d1c1fce92abecdf4e13ebb2366a568
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2024-01-12 20:31:23 -0800

    Update AppTimestampData.cs

--- Metrics-Analyzer/Data/AppTimestampData.cs
+++ Metrics-Analyzer/Data/AppTimestampData.cs
@@ -3,10 +3,10 @@
 internal class AppTimestampData
 {
     public DateTime Date { private get; set; }
-    public decimal Revenue { private set; get; }
-    public decimal MarketingSpend { private set; get; }
+    public double Revenue { private set; get; }
+    public double MarketingSpend { private set; get; }
 
-    public AppTimestampData(DateTime date, decimal revenue, decimal marketingSpend)
+    public AppTimestampData(DateTime date, double revenue, double marketingSpend)
     {
         Date = date;
         Revenue = revenue;
