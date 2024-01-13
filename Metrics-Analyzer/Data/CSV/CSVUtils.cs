05e20f277389e6c910ab04c4363ef130f8995c43
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2024-01-12 19:46:12 -0800

    Parsed CSV files

---
+++ Metrics-Analyzer/Data/CSV/CSVUtils.cs
@@ -0,0 +1,28 @@
+﻿namespace Metrics_Analyzer.Data.CSV
+{
+    static internal class CSVUtils
+    {
+        public const char LineSeparator = '\n';
+        public const char ColumnSeparator = ',';
+
+        public static string ParseString(string str) => str ?? String.Empty;
+        public static DateTime ParseDate(string str)
+        {
+            if (DateTime.TryParse(str, out var result))
+                return result;
+            return default(DateTime);
+        }
+        public static int ParseInt(string str)
+        {
+            if (int.TryParse(str, out var result))
+                return result;
+            return 0;
+        }
+        public static decimal ParseDecimal(string str)
+        {
+            if (decimal.TryParse(str, out var result))
+                return result;
+            return 0;
+        }
+    }
+}
